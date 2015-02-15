2b8e2c2026d5929811a4fcabd020353cece84d9b
Eduardo dos santos <rockero_spg@hotmail.com>
2015-02-15 18:17:42 -0200

    interface inicial com: ícones, tela inicial e guias.

---
+++ prototipoRestaurante/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  prototipoRestaurante
+//
+//  Created by Eduardo dos santos on 14/02/15.
+//  Copyright (c) 2015 megamil. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        
+        return Int(UIInterfaceOrientationMask.All.rawValue)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
