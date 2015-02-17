a2fd9b0952a306a3ec3f67abbb3e4ef1f646be71
Eduardo dos santos <rockero_spg@hotmail.com>
2015-02-17 21:25:24 -0200

    Melhoras na interface, adicionado botões: sugerir e calendário.

--- prototipoRestaurante/FirstViewController.swift
+++ prototipoRestaurante/FirstViewController.swift
@@ -8,19 +8,71 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    
+    var arrayPratos : NSMutableArray = NSMutableArray()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        for i in 1...20 {
+            
+            var prato : String = "Prato: \(i)"
+            var preco : String = "Preço: R$\(i*10),00"
+            var temparray : NSArray = NSArray(objects: prato, preco)
+            self.arrayPratos.addObject(temparray)
+            
+        }
+        
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+     
+        var id : String = "id"
+        
+        var celula : UITableViewCell? = tableView.dequeueReusableCellWithIdentifier(id) as? UITableViewCell
+        
+        if celula == nil {
+            
+            celula = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: id)
+            
+        }
+        
+        var objeto : NSArray = self.arrayPratos.objectAtIndex(indexPath.row) as NSArray
+        
+        celula?.textLabel?.text = objeto.objectAtIndex(0) as? String
+        celula?.detailTextLabel?.text = objeto.objectAtIndex(1) as? String
+        
+        var icone : UIImage? = UIImage(named: "bandeja.png")
+        
+        celula?.imageView?.image = icone
+        
+        return celula!
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayPratos.count
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     override func supportedInterfaceOrientations() -> Int {
         
         return Int(UIInterfaceOrientationMask.All.rawValue)
         
-    }
-
+    }    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
